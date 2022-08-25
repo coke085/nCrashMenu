e22be076cbdb82a0c3c0b9dfc1dd47ccd67346eb
Nonay <80323073+coke085@users.noreply.github.com>
2022-08-25 23:10:29 +0100

    Add files via upload

---
+++ lua/autorun/cm_config.lua
@@ -0,0 +1,44 @@
+//Increase this if the menu shows on map change.
+DelayTime = 5
+
+//Whats the title?
+Title = "Nonay"
+
+//What message do you want to display when the server has crashed?
+Message = "Nonay Detected a possible crash, looks like this may be an issue on your end. Try restarting gmod or your router and reconnect!"
+
+//What is the estimated time in seconds it takes for the server to restart after a crash?
+ServerRestartTime = 85
+
+BackgroundColor = Color(52, 152, 219)
+
+ButtonColor = Color(236, 240, 241)
+ButtonHoverColor = Color(41, 128, 185)
+
+TitleTextColor = Color(236, 240, 241)
+MessageTextColor = Color(236, 240, 241)
+ButtonTextColor = Color(52, 152, 219)
+
+//Insert the YouTube URL if you want music. Make sure you put it in quotes.
+//Leave it at nil if you dont want any music.
+YouTubeURL = "https://www.youtube.com/watch?v=xNjyG8S4_kI"
+-- YouTubeURL = "http://www.youtube.com/embed/Pnm8JUwSxoI"
+
+//Server buttons(Limit 4).
+ServerNameButtons = {
+	"FAQ",
+	"Discord",
+	"Server Content",
+	-- "Groupe C-Gaming Steam"
+}
+
+//Make sure it corresponds to the server names above!
+//You can also do websites. Have it start with http://
+ServerIPButtons = {
+	"https://discord.gg/8R3bvgx",
+	"https://discord.gg/8R3bvgx",
+	"https://steamcommunity.com/sharedfiles/filedetails/?id=2322127797",
+	-- "http://steamcommunity.com/groups/communitygaminffr",
+}
+
+//Delete the code inside the brackets of both the ServerNameButtons and ServerIPButtons if you don't need server buttons.
